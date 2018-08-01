bf1c96020e195fa6ab4f3cc7a64b81bc30029e57
Xichen Zhou <xzhou@xeechou.net>
2018-08-01 16:14:41 -0400

    fork-exec

--- src/os/exec.c
+++ src/os/exec.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 #include "exec.h"
 
-void fork_exec(int argc, char *argv[])
+int fork_exec(int argc, char * const argv[])
 {
 	pid_t pid = fork();
 	if (pid == -1) {
-
+		//this can happen thogh
 	} else if (pid == 0) {
 		//you don't need to setsid since you want to close all the
 		//clients as we close, also, child program can close the stdin,
@@ -15,8 +15,7 @@ void fork_exec(int argc, char *argv[])
 		//child process, we should close all the
 		//setsid, we should probably setsid, close stdin, stdout
 		//what we can do here is changing the envals.
-		execv(argv[0], &argv[1]);
-	} else {
-		return;
+		execv(argv[0], argv);
 	}
+	return pid;
 }

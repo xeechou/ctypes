e252fbe040757bbb4199fcaaa52a916125c04b6a
sichem <xeechou@gmail.com>
2018-12-20 13:54:31 -0500

    file operations

--- src/os/file.h
+++ src/os/file.h
@@ -6,26 +6,43 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <stdarg.h>
 
+//file related operations
 
-//avoid using helpers now, make them in the private namespace
-size_t
-file_read(const char *f, char *mem, int max_size)
+size_t file_read(const char *f, char *mem, int max_size);
+
+bool is_dir_empty(DIR *dir);
+
+static inline bool
+is_file_exist(const char *abs_path)
 {
-	FILE *stream = fopen(f, "r");
-	size_t cr = ftell(f);
-	fseek(stream, 0, SEEK_END);
-	size_t size = ftell(f);
-	fseek(stream, 0, SEEK_SET);
-	int _size = (max_size > size+1) ? size : max_size - 1;
-	if (mem)
-		fread(mem, sizeof(char), _size, stream);
-	mem[_size] = '\0';
-	fseek(stream, cr, SEEK_SET);
-	fclose(stream);
-	return _size;
+	FILE *f;
+	if ((f = fopen(abs_path, "r")) != NULL) {
+		fclose(f);
+		return true;
+	}
+	return false;
 }
 
+
+static inline struct dirent *
+dir_find_pattern(DIR *dir, const char *format, ...)
+{
+	struct dirent *drnt = NULL;
+
+	while ((drnt = readdir(dir)) != NULL) {
+		va_list ap;
+		int encounter = 0;
+		va_start(ap, format);
+		encounter = vsscanf(drnt->d_name, format, ap);
+		va_end(ap);
+		if (encounter != 0)
+			break;
+	}
+	return drnt;
+}
 
 
 #endif
